6a8554c160fc40e29c92db1db1e4a740ddd1e719
Abdul Dakkak <abduld@wolfram.com>
2018-10-01 12:01:46 -0400

    update

--- src/components/LandingPage_1/Introduction.js
+++ src/components/LandingPage_1/Introduction.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function makeSlide({ tag, body, compare }) {
   return (
-    <div>
+    <div style={{ minHeight: "50vh", padding: "20px", margin: "20px" }}>
       <Row type="flex">
         <Col span={6} offset={9}>
           <h1
@@ -100,8 +100,11 @@ const Message3 = (() => {
 export default class Introduction extends Component {
   render() {
     return (
-      <div className="LandingPage-introduction">
-        <Carousel vertical autoplay>
+      <div
+        className="LandingPage-introduction"
+        style={{ minHeight: "60vh", padding: "100px 0", textAlign: "center" }}
+      >
+        <Carousel autoplay dots={false}>
           {Message1}
           {Message2}
           {Message3}
