4f84500633a7e8089bf81654e5bdc2d58e027570
Abdul Dakkak <abduld@wolfram.com>
2018-10-03 05:54:57 -0400

    possibly include ie11 support

--- src/index.js
+++ src/index.js
@@ -7,5 +7,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+// import "react-app-polyfill/ie9"; // For IE 9-11 support
+// import "react-app-polyfill/ie11"; // For IE 11 support
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
