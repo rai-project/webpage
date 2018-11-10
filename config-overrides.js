49c97dfea093c93a288e060a4faba3a04b094fc5
Abdul Dakkak <abduld@wolfram.com>
2018-11-10 16:01:26 -0600

    use themes

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,29 @@
-const {
-  injectBabelPlugin
-} = require('react-app-rewired');
+const { injectBabelPlugin } = require("react-app-rewired");
+const rewireLess = require("react-app-rewire-less");
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
-    ['import', {
-      libraryName: 'antd',
-      libraryDirectory: 'es',
-      style: 'css'
-    }],
-    config,
+    [
+      "import",
+      {
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: true,
+      },
+    ],
+    config
   );
+  config = injectBabelPlugin(
+    ["import", { libraryName: "antd-mobile", libraryDirectory: "lib" }, "antd-mobile"],
+    config
+  );
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {
+      // "@primary-color": "#1a263a",
+      "@font-family": `"IBM Plex Sans", "Helvetica Neue", Arial, sans-serif`,
+      "@code-family": `"IBM Plex Mono", "Menlo", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", Courier, monospace`
+    },
+    javascriptEnabled: true,
+  })(config, env);
   return config;
 };
