ca1b6a3d218c847aff68788f6f7f71f1c8f852f2
Renee Zhu <reneezhu1994@gmail.com>
2018-11-10 17:37:36 -0600

    fix lodash config and lodash import

--- config-overrides.js
+++ config-overrides.js
@@ -22,7 +22,8 @@ module.exports = function override(config, env) {
       "libraryName": "lodash",
       "libraryDirectory": "",
       "camel2DashComponentName": false,  // default: true
-    }],
+    },
+    "lodash"],
     config
   );
   config = rewireLess.withLoaderOptions({
