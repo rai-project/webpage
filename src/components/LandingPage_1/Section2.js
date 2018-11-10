f10baaa012a7e65a7238f75fe078cd502a217c7d
Abdul Dakkak <abduld@wolfram.com>
2018-11-10 00:06:09 -0600

    link to docs

--- src/components/LandingPage_1/Section2.js
+++ src/components/LandingPage_1/Section2.js
@@ -24,7 +24,7 @@ export default class Section2 extends Component {
         </Row>
         <Row type="flex" justify="space-around">
           <Col span={12}>
-            <p style={{ fontSize: "15pt", marginTop: "20px", color: "black" }}>
+            <p style={{ fontSize: "12pt", marginTop: "20px", color: "black" }}>
               The one-stop open platform designed to spur innovation by enabling machine learning
               developers, users, and system optimizers to quickly find, test, deploy, and benchmark
               combinations of models, frameworks and hardware configurations.
@@ -33,7 +33,9 @@ export default class Section2 extends Component {
         </Row>
         <Row type="flex" justify="space-around">
           <Col>
-            <p style={{ fontSize: "8pt", marginTop: "10px", color: "#0FACAC" }}>LEARN MORE</p>
+            <p style={{ fontSize: "8pt", marginTop: "10px", color: "#0FACAC" }}>
+              <a href="docs.mlmodelscope.org">LEARN MORE</a>
+            </p>
           </Col>
         </Row>
       </div>
