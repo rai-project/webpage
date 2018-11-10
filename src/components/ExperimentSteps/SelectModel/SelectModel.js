50582ca1b91eedf115e1e09af1c04834aad770df
Abdul Dakkak <abduld@wolfram.com>
2018-11-10 06:48:39 -0600

    tweaks

--- src/components/ExperimentSteps/SelectModel/SelectModel.js
+++ src/components/ExperimentSteps/SelectModel/SelectModel.js
@@ -1,13 +1,29 @@
 import SelectableCard from "../SelectableCard/SelectableCard";
 import React, { Component } from "react";
-import { Col, Row, Layout, Card, Avatar, Icon } from "antd";
+import { Col, Row, Layout, Icon, Divider, Dropdown, Menu } from "antd";
 import yeast from "yeast";
 import { withRouter } from "react-router-dom";
-import { isArray, keys, uniqBy, find, findIndex, isNil, truncate } from "lodash";
+import { map, isArray, keys, uniqBy, find, findIndex, isNil, truncate } from "lodash";
 import { ModelManifests } from "../../../swagger";
 import { ExperimentContext } from "../../../context/ExperimentContext";
 
 const { Content } = Layout;
+
+function typeRender({ type }) {
+  if (type === "image") {
+    return <Icon key={yeast()} type="picture" />;
+  }
+  if (type === "feature") {
+    return <Icon key={yeast()} type="appstore" />;
+  }
+  if (type === "text") {
+    return <Icon key={yeast()} type="file-text" />;
+  }
+  if (type === "audio") {
+    return <Icon key={yeast()} type="sound" />;
+  }
+  return <Icon key={yeast()} type="cluster" />;
+}
 
 class SelectModel extends Component {
   constructor(props) {
@@ -73,7 +89,7 @@ class SelectModel extends Component {
               Select a model
             </h2>
           </div>
-          <Row gutter={16}>
+          <Row gutter={16} type="flex" justify="space-around" align="middle">
             {modelsKey.map(key => {
               const model = models[key];
               const isSelected = !isNil(
@@ -82,20 +98,62 @@ class SelectModel extends Component {
                   e => e.name === model.name && e.version === model.version
                 )
               );
+
+              const menu = (
+                <Menu>
+                  <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+                      1st menu item
+                    </a>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+                      2nd menu item
+                    </a>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+                      3rd menu item
+                    </a>
+                  </Menu.Item>
+                </Menu>
+              );
               return (
-                <Col key={`model-${key}`} span={6} style={{ paddingBottom: "10px" }}>
+                <Col
+                  key={`model-${key}`}
+                  span={8}
+                  style={{ paddingBottom: "10px", paddingTop: "10px" }}
+                >
                   <SelectableCard
-                    title={models[key].name}
-                    content={truncate(models[key].description, {
+                    title={model.name}
+                    content={truncate(model.description, {
                       length: 140,
                       separator: " ",
                     })}
                     descriptionTitle={`${model.name} Information`}
                     description={model.description}
-                    height="150px"
+                    height="100%"
                     onClick={() => this.handleSelect(isSelected, key)}
                     selected={isSelected}
-                  />
+                  >
+                    <Row type="flex" justify="end" align="bottom">
+                      <Divider orientation="right">
+                        <div style={{ color: "#aaa", fontSize: "10pt" }}>Options</div>
+                      </Divider>
+                      <Col span={8}>
+                        ({map(model.inputs, typeRender)}) → {typeRender(model.output)}
+                      </Col>
+                      <Col span={8} />
+
+                      <Col span={8}>
+                        <Dropdown overlay={menu}>
+                          <a href="#">
+                            data type <Icon type="down" />
+                          </a>
+                        </Dropdown>
+                      </Col>
+                    </Row>
+                  </SelectableCard>
                 </Col>
               );
             })}
