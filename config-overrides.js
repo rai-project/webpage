5c70f260f36b2eaf01083e31d54ec1e1718f4d6b
Abdul Dakkak <abduld@wolfram.com>
2018-11-10 16:11:57 -0600

    updates

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const { injectBabelPlugin } = require("react-app-rewired");
 const rewireLess = require("react-app-rewire-less");
+const rewireIdx = require("react-app-rewire-idx");
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
@@ -17,21 +18,26 @@ module.exports = function override(config, env) {
     ["import", { libraryName: "antd-mobile", libraryDirectory: "lib" }, "antd-mobile"],
     config
   );
-  config = injectBabelPlugin(
-    ["import", {
-      "libraryName": "lodash",
-      "libraryDirectory": "",
-      "camel2DashComponentName": false,  // default: true
-    }],
-    config
-  );
+  // config = injectBabelPlugin(
+  //   [
+  //     "import",
+  //     {
+  //       libraryName: "lodash",
+  //       libraryDirectory: "",
+  //       camel2DashComponentName: false, // default: true
+  //     },
+  //   ],
+  //   config
+  // );
   config = rewireLess.withLoaderOptions({
     modifyVars: {
       // "@primary-color": "#1a263a",
       "@font-family": `"IBM Plex Sans", "Helvetica Neue", Arial, sans-serif`,
-      "@code-family": `"IBM Plex Mono", "Menlo", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", Courier, monospace`
+      "@code-family": `"IBM Plex Mono", "Menlo", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", Courier, monospace`,
     },
     javascriptEnabled: true,
   })(config, env);
+
+  config = rewireIdx(config, env);
   return config;
 };
