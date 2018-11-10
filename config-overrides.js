e476ae99aa2e3292f7201a9be7a593e43ec00d50
Abdul Dakkak <abduld@wolfram.com>
2018-11-10 16:03:39 -0600

    update lodash

--- config-overrides.js
+++ config-overrides.js
@@ -17,6 +17,14 @@ module.exports = function override(config, env) {
     ["import", { libraryName: "antd-mobile", libraryDirectory: "lib" }, "antd-mobile"],
     config
   );
+  config = injectBabelPlugin(
+    ["import", {
+      "libraryName": "lodash",
+      "libraryDirectory": "",
+      "camel2DashComponentName": false,  // default: true
+    }],
+    config
+  );
   config = rewireLess.withLoaderOptions({
     modifyVars: {
       // "@primary-color": "#1a263a",
